36bbd2620832544285eafebe4ce49efbd8ee057b
Murugan <murugu2012@gmail.com>
2016-11-11 19:41:54 +0530

    Direct association between restaurants and reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,10 @@
 class Restaurant < ApplicationRecord
   # Direct associations
 
+  has_many   :photos,
+             :class_name => "Review",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
